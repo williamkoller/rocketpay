6928a4f9064067a49c90f22800e170f8eec6d028
william koller <wkoller25@gmail.com>
2021-02-22 23:01:08 -0300

    feat: add WelcomeController call index

--- config/dev.exs
+++ config/dev.exs
@@ -2,9 +2,9 @@ use Mix.Config
 
 # Configure your database
 config :rocketpay, Rocketpay.Repo,
-  username: "postgres",
-  password: "postgres",
-  database: "rocketpay_dev",
+  username: "docker",
+  password: "root",
+  database: "elixir_db",
   hostname: "localhost",
   show_sensitive_data_on_connection_error: true,
   pool_size: 10
--- config/test.exs
+++ config/test.exs
@@ -6,9 +6,9 @@ use Mix.Config
 # to provide built-in test partitioning in CI environment.
 # Run `mix help test` for more information.
 config :rocketpay, Rocketpay.Repo,
-  username: "postgres",
-  password: "postgres",
-  database: "rocketpay_test#{System.get_env("MIX_TEST_PARTITION")}",
+  username: "docker",
+  password: "root",
+  database: "elixir_db",
   hostname: "localhost",
   pool: Ecto.Adapters.SQL.Sandbox
 
